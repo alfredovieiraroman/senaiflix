4434bb943ea2040b4878907a03e00a6e77f9ba6d
Alfredo Roman <58364328+alfredovieiraroman@users.noreply.github.com>
2020-06-02 08:00:30 -0300

    upado

--- SRC/routes.js
+++ SRC/routes.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
-import Cursos from './pages/cursos';
+import Cursos from "./pages/cursos";
 
 const AppStack = createStackNavigator();
 
